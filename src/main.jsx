19836736c72707541ca4335c14aa71b7f9e9ed13
Paris Scrivens <parisscrivens@hotmail.com>
2025-05-07 21:32:45 +0100

    Scaffolded component folders and placeholder components

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+
+import Playlist from './components/Playlist/Playlist'
+import SearchBar from './components/SearchBar/SearchBar'
+import SearchResults from './components/SearchResults/SearchResults'
+import Track from './components/Track/Track'
+import TrackList from './components/TrackList/TrackList'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Playlist />
+    <SearchBar />
+    <SearchResults />
+    <Track />
+    <TrackList />
   </StrictMode>,
 )
